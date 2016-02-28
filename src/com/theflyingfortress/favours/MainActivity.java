dcb5aec9df793c83a5d8eae2d3e939773d2b6f4f
BlkNoiz <procintu72@hotmail.com>
2016-02-27 20:03:05 -0800

    Facebook SDK
    Direct changes made to http://puu.sh/no4T8/829eb35f6f.png Confirm
    buildToolVersion and compileSDKVersion later

--- src/com/theflyingfortress/favours/MainActivity.java
+++ src/com/theflyingfortress/favours/MainActivity.java
@@ -7,6 +7,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import com.facebook.FacebookSdk;
 
 public class MainActivity extends Activity {
 
@@ -49,5 +50,19 @@ public class MainActivity extends Activity {
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		// Logs 'install' and 'app activate' App Events.
+		AppEventsLogger.activateApp(this);
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+		// Logs 'app deactivate' App Event.
+		AppEventsLogger.deactivateApp(this);
 	}
 }
