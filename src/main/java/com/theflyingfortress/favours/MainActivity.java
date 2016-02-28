b6823d7b761e6d205e40f47b588b4686716d0f1b
BlkNoiz <procintu72@hotmail.com>
2016-02-28 09:34:02 -0800

    added fb login button

--- src/main/java/com/theflyingfortress/favours/MainActivity.java
+++ src/main/java/com/theflyingfortress/favours/MainActivity.java
@@ -16,7 +16,7 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
-
+        FacebookSdk.sdkInitialize(getApplicationContext());
         setContentView(R.layout.activity_main);
         
         /*
